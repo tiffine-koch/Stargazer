62c60f42a30a793f235b9114145d9ba225bc7764
Apple <tiffine.koch@gmail.com>
2016-02-27 00:11:37 -0800

    re-roll working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var $num, randNum, winNum, winArray = [], checkWinArray = [], reRoll = 3;
+var $num, randNum, winNum, winArray = [], checkWinArray = [], reRoll = 0, gameplay = true;
 
 $(document).ready(init);
 
@@ -21,12 +21,17 @@ function submit() {
 
 function reroll() {
   createStars();
-  reRoll = reRoll--;
-  console.log('reroll');
-
+  var count = reRoll++;
+  console.log('reRoll', count);
+  if(count === 2) {
+    console.log('game over');
+  } else {
+    console.log('reroll');
+    createStars();
+  }
 }
 
-function randNumStars() {
+function gameOver() {
 
 }
 
@@ -34,36 +39,51 @@ function createStars() {
   console.log('create');
   randNum = Math.ceil(Math.random() * 8);
   console.log("randNum", randNum);
+
   for(var i = 0; i < randNum; i++) {
     var $star = $('<div>').addClass('fa fa-star star fa-5x');
     $('#holder').append($star);
+    // $('#displayCount').text(randNum + " stars").addClass('count');
   }
 
   // numberArray = numberArray.concat();
   // var randomArray = [];
 }
 
+// $something.empty();
+
 function starClick() {
   console.log('starClick');
-
+  //animate css
 }
+
 function numClick() {
   console.log('numClick');
-  var $num = ($(this).text());
+  var $this = $(this);
+  var $num = $this.text();
   console.log($num);
 
+  var wasSelected = $this.hasClass('selected');
+  if(!wasSelected) {
+    $this.addClass('selected');
+  }
 }
 
 function checkForRoundWin() {
   console.log('round');
+  //createStars();
 
 }
 
 function checkForGameWin() {
+  //if winner remove selected
   console.log('game');
 }
 
 function restart() {
   console.log('restart');
-  document.location.href = "";
+  createStars();
+  reRoll = 3;
+  //remove selected
+  // document.location.href = "";
 }
